3b3566bde57bdebd4097e09289309420d39e33cb
RickyM7 <24703825+RickyM7@users.noreply.github.com>
2025-09-16 20:44:56 -0300

    chore: fila para falhas por token expirado

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,17 @@
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
 async function refreshToken() {
   const config = useRuntimeConfig();
   try {
@@ -32,20 +46,38 @@ export async function authenticatedFetch(url, options = {}) {
 
   let response = await fetch(url, { ...options, headers });
 
-  // Se a requisição falhar com 401 (Unauthorized), tenta renovar o token
   if (response.status === 401) {
-    const newToken = await refreshToken();
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        failedQueue.push({ resolve, reject });
+      })
+      .then(newToken => {
+        headers['Authorization'] = `Bearer ${newToken}`;
+        return fetch(url, { ...options, headers });
+      })
+      .catch(err => {
+        return Promise.reject(err);
+      });
+    }
 
-    if (newToken) {
-      // Se a renovação for bem-sucedida, atualiza os headers e tenta a requisição novamente
-      headers['Authorization'] = `Bearer ${newToken}`;
-      response = await fetch(url, { ...options, headers });
-    } else {
-      // Se a renovação falhar, desloga o usuário
-      localStorage.removeItem('access');
-      localStorage.removeItem('user_data');
-      window.location.href = '/login';
-      throw new Error('Sua sessão expirou. Por favor, faça login novamente.');
+    isRefreshing = true;
+
+    try {
+      const newToken = await refreshToken();
+
+      if (newToken) {
+        processQueue(null, newToken);
+        headers['Authorization'] = `Bearer ${newToken}`;
+        response = await fetch(url, { ...options, headers });
+      } else {
+        processQueue(new Error('Sua sessão expirou. Por favor, faça login novamente.'));
+        localStorage.removeItem('access');
+        localStorage.removeItem('user_data');
+        window.location.href = '/login';
+        throw new Error('Sua sessão expirou. Por favor, faça login novamente.');
+      }
+    } finally {
+      isRefreshing = false;
     }
   }
 
